e20c63c9f8d4d40008c1b68a2cf2ed41986fea06
Aram Baghdassarian <abagh0703@gmail.com>
2019-09-19 21:32:40 -0400

    added quick-select for wakeup time

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import {Root, Container, Header, Left, Body, Right, Title, Content} from 'native-base';
+import {
+  Root, Container, Header, Left, Body, Right, Title, Content,
+} from 'native-base';
 import ArrowView from './routes/ArrowView';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <Root>
-            <Container>
-                <Header>
-                    <Left/>
-                    <Body>
-                    <Title>Paris</Title>
-                    </Body>
-                    <Right/>
-                </Header>
-                <Content padder>
-                    <ArrowView/>
-                </Content>
-            </Container>
-            </Root>
-        );
-    }
+  render() {
+    return (
+      <Root>
+        <Container>
+          <Header>
+            <Left />
+            <Body>
+              <Title>Paris</Title>
+            </Body>
+            <Right />
+          </Header>
+          <Content padder>
+            <ArrowView />
+          </Content>
+        </Container>
+      </Root>
+    );
+  }
 }
--- routes/ArrowView.js
+++ routes/ArrowView.js
@@ -1,6 +1,9 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { Content, Text } from 'native-base';
+import {
+  Content, Text, Button, Left, Toast,
+} from 'native-base';
+// import { Col, Row, Grid } from 'react-native-easy-grid';
 import { getApiUrl } from '../components/Variables';
 import Arrow from '../components/Arrow';
 
@@ -13,8 +16,6 @@ export default class ArrowView extends React.Component {
   }
 
   componentDidMount() {
-    console.log('getting');
-    console.log(`${getApiUrl()}/arrows/`);
     setInterval(() => {
       fetch(`${getApiUrl()}/arrows/`)
         .then(response => response.json())
@@ -38,12 +39,69 @@ export default class ArrowView extends React.Component {
         .catch((error) => {
           console.error(error);
         });
-    }, 20000);
+    }, 10000);
   }
+
+  createArrowHoursAhead(numHoursAhead) {
+    const currentMsTime = ((new Date()).getTime());
+    const numHoursInMs = this.convertHoursToMs(numHoursAhead);
+    this.createNewBeHomeArrow(currentMsTime + numHoursInMs);
+    Toast.show({
+      text: `Arrow for ${numHoursAhead} created.`,
+      buttonText: 'Okay',
+      duration: 1500,
+    });
+  }
+
+  convertHoursToMs(hours) {
+    return (hours * 60 * 60 * 1000);
+  }
+
+  createNewBeHomeArrow(checkInTime) {
+    const DRYDEN_LAT = 42.442136;
+    const DRYDEN_LONG = -76.484368;
+    fetch(`${getApiUrl()}/arrows/`, {
+      method: 'post',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        latitude: DRYDEN_LAT,
+        longitude: DRYDEN_LONG,
+        checkInTime,
+        dateType: 'once',
+        arrowType: 'leaveSomewhere',
+        label: 'Don\'t be home',
+      }),
+    }).then(response => response.json())
+      .then((res) => {
+        this.setState(prevState => (
+          {
+            arrows: [...prevState.arrows, res],
+          }
+        ));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
 
   render() {
     return (
       <Content>
+        <Text>Quick-Select How Long You Want To Sleep:</Text>
+        <Left>
+          {[6, 6.25, 6.75, 7, 7.25, 7.75, 8, 8.25, 8.75, 9].map(numHours => (
+            <Button light onPress={() => this.createArrowHoursAhead(numHours)}>
+              <Text>
+                {numHours}
+              </Text>
+            </Button>
+          ))}
+        </Left>
+        <Text>Arrows</Text>
         {this.state.arrows}
       </Content>
     );
